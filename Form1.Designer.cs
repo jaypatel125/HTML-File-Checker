a0e91cbb6a930cb90a24c1ffe08bb4b1fbd9abf6
Jay Patel <000881881@mohawkcollege.ca>
2024-04-05 11:36:09 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Lab4B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkTagsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(614, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // loadFileToolStripMenuItem
+            // 
+            this.loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
+            this.loadFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.loadFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.loadFileToolStripMenuItem.Text = "Load File";
+            this.loadFileToolStripMenuItem.Click += new System.EventHandler(this.loadFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkTagsToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // checkTagsToolStripMenuItem
+            // 
+            this.checkTagsToolStripMenuItem.Enabled = false;
+            this.checkTagsToolStripMenuItem.Name = "checkTagsToolStripMenuItem";
+            this.checkTagsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.checkTagsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.checkTagsToolStripMenuItem.Text = "Check Tags";
+            this.checkTagsToolStripMenuItem.Click += new System.EventHandler(this.checkTagsToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "No File Loaded";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(30, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(553, 372);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 481);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Jay\'s simple HTML file checker";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkTagsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿/*
+Author: Jay Patel, 000881881
+Date: 15/11/2023
+Purpose: Lab 4 Part B
+I, Jay Patel, 000881881 certify that this material is my original work.  No other person's work has been used without due acknowledgement.
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Lab4B
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFilePath;
+        private string filename;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the "Load File" menu item.
+        /// Opens a file dialog to load an HTML file.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void loadFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Clear the list box before loading a new file
+            listBox1.Items.Clear();
+
+            // Open file dialog to select an HTML file
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "HTML Files(*.html;*.htm) | *.html;*.htm";
+
+            // If the user selects a file, enable the "Check Tags" menu item
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                checkTagsToolStripMenuItem.Enabled = true;
+                selectedFilePath = openFileDialog.FileName;
+                filename = Path.GetFileName(selectedFilePath);
+
+                // Display the loaded filename in the label
+                label1.Text = ($"Loaded: {filename}");
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the "Check Tags" menu item.
+        /// Checks if the HTML file has balanced tags.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void checkTagsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Check if the HTML tags are balanced
+            bool tagsBalanced = CheckTags(selectedFilePath);
+
+            // Update the label based on the result of tag checking
+            if (tagsBalanced)
+            {
+                label1.Text = ($"{filename} has balanced tags.");
+            }
+            else
+            {
+                label1.Text = ($"{filename} does not have balanced tags.");
+            }
+        }
+
+        /// <summary>
+        /// Checks if the HTML file has balanced tags.
+        /// </summary>
+        /// <param name="filePath">The path to the HTML file.</param>
+        /// <returns>True if tags are balanced; otherwise, false.</returns>
+        private bool CheckTags(string filePath)
+        {
+            try
+            {
+                // Read the HTML file content
+                string htmlContent = File.ReadAllText(filePath);
+
+                // Initialize counters for opening and closing tags
+                int openingTagCount = 0;
+                int closingTagCount = 0;
+
+                // Define a stack to keep track of opening tags and their nesting level
+                Stack<string> tagStack = new Stack<string>();
+
+                // Define a regular expression to match HTML tags
+                Regex tagRegex = new Regex(@"<[^>]+>");
+
+                // Match all HTML tags in the content
+                MatchCollection tagMatches = tagRegex.Matches(htmlContent);
+
+                // Process each matched tag
+                foreach (Match match in tagMatches)
+                {
+                    string tag = match.Value;
+
+                    // Check if the tag is an opening tag
+                    if (tag.StartsWith("<") && !tag.StartsWith("</"))
+                    {
+                        // Ignore certain tags
+                        if (!IsIgnoredTag(tag))
+                        {
+                            // Increment the opening tag counter
+                            openingTagCount++;
+
+                            // Get the tag name without attributes
+                            string tagName = GetTagName(tag).ToLower();
+
+                            // Indentation for display
+                            string indentation = new string(' ', tagStack.Count * 2);
+
+                            listBox1.Items.Add($"{indentation}Found opening tag: <{tagName}>");
+
+                            // Push the opening tag onto the stack
+                            tagStack.Push(tagName);
+                        }
+                        else
+                        {
+                            // If it's an ignored tag, use the same indentation
+                            string indentation = new string(' ', tagStack.Count * 2);
+                            string tagName = GetTagName(tag).ToLower();
+                            listBox1.Items.Add($"{indentation}Found opening tag: <{tagName}>");
+                        }
+                    }
+                    else if (tag.StartsWith("</"))
+                    {
+                        // Increment the closing tag counter
+                        closingTagCount++;
+
+                        // Get the tag name without attributes
+                        string tagName = GetTagName(tag).ToLower();
+
+                        // If the stack is not empty, check if the last opening tag corresponds
+                        if (tagStack.Count > 0)
+                        {
+                            var poppedTag = tagStack.Pop();
+
+                            // Indentation for display
+                            string indentation = new string(' ', tagStack.Count * 2);
+
+                            listBox1.Items.Add($"{indentation}Found closing tag: <{tagName}>");
+                        }
+                        else
+                        {
+                            // If the stack is empty, there is no corresponding opening tag
+                            return false;
+                        }
+                    }
+                }
+
+                // If both counters are equal, all tags are balanced
+                if (openingTagCount == closingTagCount)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                label1.Text = (ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks if a tag should be ignored.
+        /// </summary>
+        /// <param name="tag">The tag to check.</param>
+        /// <returns>True if the tag should be ignored; otherwise, false.</returns>
+        private bool IsIgnoredTag(string tag)
+        {
+            // Define a list of tags to be ignored
+            List<string> ignoredTags = new List<string> { "<img", "<hr", "<br" };
+
+            // Check if the tag is in the list of ignored tags
+            return ignoredTags.Any(ignoredTag => tag.StartsWith(ignoredTag));
+        }
+
+        /// <summary>
+        /// Gets the tag name without attributes.
+        /// </summary>
+        /// <param name="tag">The full tag.</param>
+        /// <returns>The tag name.</returns>
+        private string GetTagName(string tag)
+        {
+            // Remove attributes from the tag to get only the tag name
+            int indexOfSpace = tag.IndexOf(' ');
+            if (indexOfSpace != -1)
+            {
+                return tag.Substring(1, indexOfSpace - 1);
+            }
+            else
+            {
+                // If no space is found, remove the closing angle bracket
+                return tag.Substring(1, tag.Length - 2);
+            }
+        }
+
+
+        // Exits the application
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
